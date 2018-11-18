e52c548dbeb9da7a075b030719ec20ce49703ec7
vuvanlinh <vuvanlinh95@gmail.com>
2018-11-18 22:05:37 +0700

    Mylist

---
+++ src/MyList/MyList.java
@@ -0,0 +1,34 @@
+package MyList;
+
+import java.util.Arrays;
+
+class MyList<E> {
+    private int size = 0;
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object elements[];
+
+    MyList() {
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+
+    private void ensureCapa() {
+        int newSize = elements.length * 2;
+        elements = Arrays.copyOf(elements, newSize);
+    }
+
+    void add(E e) {
+        if (size == elements.length) {
+            ensureCapa();
+        }
+        elements[size++] = e;
+    }
+
+    E get(int i) {
+        if (i>= size || i <0) {
+            throw new IndexOutOfBoundsException("Index: " + i + ", Size " + i );
+        }
+        return (E) elements[i];
+    }
+
+
+}
